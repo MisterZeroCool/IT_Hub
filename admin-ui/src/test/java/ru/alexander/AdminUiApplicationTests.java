d5233e575b3e500224ac57036986d50840566bb7
Fox <gir_for@mail.ru>
2022-09-14 14:38:39 +0300

    Создание основного шаблона проекта, с добавлением модулей admin-ui, database, ui

---
+++ admin-ui/src/test/java/ru/alexander/AdminUiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class AdminUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
